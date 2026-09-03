94a21bb8e357b568369b4458b1173e1b81bbf8fa
usausa <machi.pon@gmail.com>
2026-09-03 19:49:43 +0900

    Update

--- DiskInfo/MainWindowViewModel.cs
+++ DiskInfo/MainWindowViewModel.cs
@@ -9,8 +9,6 @@ public sealed class MainWindowViewModel
 
     public MainWindowViewModel(IDiskInfoProvider provider)
     {
-#pragma warning disable IDE0028
-        Disks = new ObservableCollection<DiskData>(provider.GetDisks());
-#pragma warning restore IDE0028
+        Disks = [.. provider.GetDisks()];
     }
 }
